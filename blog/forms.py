ae0e2ac267eea0cee0bbe716e7a035c74aae4439
b0bengo <jsobeng@outlook.com>
2025-03-22 20:10:49 +0000

    Enhance post and comment forms with Bootstrap styling; improve user posts layout and pagination

--- blog/forms.py
+++ blog/forms.py
@@ -5,6 +5,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Content'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
